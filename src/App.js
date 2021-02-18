76d45fe58d36a59e74c6d891dd66db7728a8b344
dongbui <dongbui303@gmail.com>
2021-02-18 07:45:57 +0700

    asdsadasd

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name ="Dong";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>123</h2>
+        <h2>xin chao {name}</h2>
       </header>
     </div>
   );
